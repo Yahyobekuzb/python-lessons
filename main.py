93d4f7dfde26e20309e6d1052250681e91888f89
YakhyobekDev <107963126+Yahyobekuzb@users.noreply.github.com>
2022-12-21 14:50:26 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-numerator = int(input("Enter a numerator: "))
-denominator = int(input("Enter a denominator: "))
-gcd_list = []
-gcd = 0
-if numerator > denominator:
-    for i in range(denominator):
-        if numerator % (i + 1) == 0 and denominator % (i + 1) == 0:
-            gcd_list.append(i + 1)
-else:
-    for i in range(numerator):
-        if numerator % (i + 1) == 0 and denominator % (i + 1) == 0:
-            gcd_list.append(i + 1)
-for i in gcd_list:
-    if i > gcd:
-        gcd = i
-print(f"result: {numerator}/{denominator} = {numerator // gcd}/{denominator // gcd}")
+from googletrans import Translator
+
+translator = Translator()
+
+matn = "Salom"
+tarjima = translator.translate(matn, src='uz', dest='en')
+
+print(tarjima.text)
